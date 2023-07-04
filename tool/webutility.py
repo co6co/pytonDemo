216b89e2c31f4324f5df3035bb972919cc596f22
co6co <co6co@qq.com>
2023-07-04 20:27:12 +0800

    dd

--- tool/webutility.py
+++ tool/webutility.py
@@ -14,6 +14,7 @@ def get(url,timeout:int|tuple|None=None):
         'http': 'http://127.0.0.1:9666',
         'https': 'https://127.0.0.1:9666'
     }
+    proxies={}
     headers={
         #"Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
         #"Accept-Encoding":"gzip, deflate, br",
@@ -31,8 +32,7 @@ def get(url,timeout:int|tuple|None=None):
         "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
         #'User-Agent':'BaiduSipder'
     }
-    if timeout == None:
-        timeout=(5, 15)
+    if timeout == None:timeout=(5, 15)
     resp = requests.get(url,headers=headers, timeout=timeout,allow_redirects=True,proxies=proxies)
     if len(resp.history) > 0: # 存在302 跳转
         location_url = resp.history[len(resp.history) - 1].headers.get('Location')
