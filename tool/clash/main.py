ce3879cb1faa6b5ab2fa3ad1f955cb254285ed41
co6co <64390596+co6co@users.noreply.github.com>
2023-07-17 20:24:00 +0800

    Update main.py

--- tool/clash/main.py
+++ tool/clash/main.py
@@ -21,7 +21,7 @@ def readJsonFile(filePath):
     with  open(filePath,"r",encoding='utf-8') as f:
         result=json.load(f) 
         log.info(len(result['data']))
-        log.info(vars(result))
+        log.info(vars(result['data']))
         return result
 
 def writeJsonFile(filePath,obj):
